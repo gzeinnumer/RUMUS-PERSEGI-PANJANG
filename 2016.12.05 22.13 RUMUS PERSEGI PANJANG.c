c1d8082a69a5bd129a76b5fba27e674384213049
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-03-29 22:57:02 +0700

    Initial commit

---
+++ 2016.12.05 22.13 RUMUS PERSEGI PANJANG.c
@@ -0,0 +1,26 @@
+/* M FADLI ZEIN
+2016/12/04 */
+
+#include<stdio.h>
+main()
+{
+    int p,le,kll,l;
+
+    printf("          $----------------------------------------------$        \n");
+    printf("        $  **********************************************  $      \n");
+    printf("       $  *****************>>GZEINNUMER<<*****************  $     \n");
+    printf("      $  ***************>>LINE: gzeinnumer<<***************  $    \n");
+    printf("      $  ****************>>BBM: 53ADE76E<<*****************  $    \n");
+    printf("       $  ***************>>0896-38900246<<****************  $     \n");
+    printf("        $  **********************************************  $      \n");
+    printf("          $----------------------------------------------$        \n\n");
+    printf("                           PERSEGI PANJANG                           \n\n");
+    printf("\t MASUKAN PANJANG : "); scanf("%d",&p);
+    printf("\t MASUKAN LEBAR   : "); scanf("%d",&le);
+    l=p*le;
+    kll=2*(p+le);
+    printf("\t HASIL\n");
+    printf("\t KELILING : %d cm\n",kll);
+    printf("\t LUAS     : %d cm\n",l);
+}
+
